aa675d832a325dc30cfdf4e6d454c6cef0f2578b
hejiawang <952327407@qq.com>
2016-11-08 13:09:17 +0800

    domain url

--- src/main/java/com/wang/core/util/DomainUrlUtil.java
+++ src/main/java/com/wang/core/util/DomainUrlUtil.java
@@ -33,6 +33,45 @@ public class DomainUrlUtil {
 	 */
 	public static String COOKIE_DOMAIN;
 	
+	/**
+	 * js模板handlebars.js用到的hbs文件目录
+	 */
+	public static String TPL_DOMAIN;
+	
+	/**
+	 * search-web URL : solr、memcached等非数据库搜索工程
+	 */
+	public static String SEARCH_DOMAIN;
+	
+	/**
+	 * 权限管理系统URL
+	 */
+	public static String PERMISSION_DOMAIN;
+	
+	public static String getPERMISSION_DOMAIN() {
+		return PERMISSION_DOMAIN;
+	}
+
+	public static void setPERMISSION_DOMAIN(String pERMISSION_DOMAIN) {
+		PERMISSION_DOMAIN = pERMISSION_DOMAIN;
+	}
+
+	public static String getSEARCH_DOMAIN() {
+		return SEARCH_DOMAIN;
+	}
+
+	public static void setSEARCH_DOMAIN(String sEARCH_DOMAIN) {
+		SEARCH_DOMAIN = sEARCH_DOMAIN;
+	}
+
+	public static String getTPL_DOMAIN() {
+		return TPL_DOMAIN;
+	}
+
+	public static void setTPL_DOMAIN(String tPL_DOMAIN) {
+		TPL_DOMAIN = tPL_DOMAIN;
+	}
+
 	public String getCOOKIE_DOMAIN() {
 		return COOKIE_DOMAIN;
 	}
